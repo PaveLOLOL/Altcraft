b742af5b32d7e309d0724753c344a6cbd155f763
Pavel <mikhajlin2010@yandex.ru>
2022-04-17 16:58:17 +0300

    Add function to scroll table (lazy loading)

--- titanic.js
+++ titanic.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const getData = async (url) => {
     const res = await fetch(url)
     const json = await res.json()
@@ -11,4 +13,23 @@ const main = async () => {
     console.log(data)
 }
 
-main();
+if (typeof main() === 'object') {
+    const passengerArrayObj = main()
+    document.querySelector('.content').innerHTML = `<table class='passenger'></table>`
+}
+
+
+// для работы с загрузкой таблицы lazy loading
+
+let intersectionObserver = new IntersectionObserver(function(entries) {
+    if (entries[0].intersectionRatio <= 0) return;
+  
+    loadItems(15);
+    console.log('Loaded new items');
+  });
+ 
+  intersectionObserver.observe(document.querySelector('.scrollerFooter'));
+
+  // todo 1) проработать ошибки с связанные с CORS, 
+  // todo 2) посмотреть как отрабатывает querySelector('.content'),
+  // todo 3) в entries должен поступать колличество записей в таблице.
